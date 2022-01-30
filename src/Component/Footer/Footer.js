e4993ec4e91ae6664843515e67ea2d9f086fa5c3
Meet Patel <patel00446@gmail.com>
2022-01-30 00:06:48 -0500

    footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -7,11 +7,7 @@ export default class Footer extends Component {
       <footer>
         <div class="footer-content">
           <h3>Software Developer</h3>
-          <p>
-            Raj Template is a blog website where you will find great tutorials
-            on web design and development. Here each tutorial is beautifully
-            described step by step with the required source code.
-          </p>
+          <p>lorem</p>
         </div>
         <div class="footer-bottom">
           <p>
